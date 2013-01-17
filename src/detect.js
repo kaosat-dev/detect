a0804bcdbe5a8f39ac7878082f2773ba6587c11f
Tim Cameron Ryan <id@timryan.org>
2013-01-17 07:08:29 -0500

    Detect module

--- src/detect.js
+++ src/detect.js
@@ -4,8 +4,10 @@
   var isBrowser = typeof window !== 'undefined';
   var isModule = typeof module !== 'undefined' && !!module.exports;
 
+  console.log(isNode, isBrowser, isModule);
+
   // Export.
-  var exports = (isModule || (this.detect = {}));
+  var exports = (isModule ? exports : (this.detect = {}));
   exports.isNode = isNode;
   exports.isBrowser = isBrowser;
   exports.isModule = isModule;
